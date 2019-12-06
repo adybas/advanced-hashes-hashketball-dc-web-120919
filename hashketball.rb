b64b7fee4eb9a7d92e676f2d8ababd0d2d17500c
Anna Dybas <a.m.dybas@gmail.com>
2019-12-06 19:03:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,9 +143,9 @@ end
 
 def player_numbers(team_name)
   nums = []
-  game_hash.each do |_place, team|
-    next unless team[:team_name] == team_name
-    team.each do |attribute, data|
+  game_hash.each do |location, info|
+    next unless info[:team_name] == team_name
+    info.each do |attribute, data|
       next unless attribute == :players
       data.each do |data|
         nums << data[:number]

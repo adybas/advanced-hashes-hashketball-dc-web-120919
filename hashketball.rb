71c69442ad74644a6264ca08731a72da49f4efcc
Anna Dybas <a.m.dybas@gmail.com>
2019-12-06 19:04:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -175,7 +175,7 @@ def big_shoe_rebounds
   biggest_shoe = 0
   num_rebounds = 0
   game_hash.each do |location, data|
-    game_data[:players].each do |player|
+    data[:players].each do |player|
       if player[:shoe] > biggest_shoe
         biggest_shoe = player[:shoe]
         num_rebounds = player[:rebounds]
